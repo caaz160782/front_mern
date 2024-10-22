f9c424ba05a1f1ddd33d8d3b2e5fcc4ad65bebde
carlos <caaz160782@gmail.com>
2024-10-21 21:16:56 -0600

    logo

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,8 +1,16 @@
 import {Outlet} from 'react-router-dom'
+import Logo from '../components/Logo'
+
 const AppLayout = () => {
   return (
     <>
-        <div>AppLayout</div>
+        <header className='bg-gray-800 py-5 '>
+          <div className='max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
+            <div className='w-64 '>
+            <Logo/>
+            </div>
+          </div>
+        </header>
         <Outlet/>
     </>
   )
