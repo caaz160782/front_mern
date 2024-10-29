555c5a27bff5dad1a4ad92c237aed730d434fb80
carlos <caaz160782@gmail.com>
2024-10-28 23:00:25 -0600

    axios files

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,12 @@
+import api from "@/lib/axios";
 import { ProjectFormData } from "../types";
 
-export async function createProject(data:ProjectFormData) {
+export async function createProject(formData:ProjectFormData) {
+ try {
+    const {data}= await api.post('/projects',formData)
     console.log(data)
+
+ } catch (error) {
+    console.log(error)
+ }
 }
