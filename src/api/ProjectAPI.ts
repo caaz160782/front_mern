988e03616eb51bc9944ebdee7c99af2c17235e9d
carlos <caaz160782@gmail.com>
2024-11-07 22:07:30 -0600

    add tanstack usemutation

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,13 +1,16 @@
 import api from "@/lib/axios";
 import { ProjectFormData } from "../types";
+import { AxiosError, isAxiosError } from "axios";
 
 export async function createProject(formData:ProjectFormData) {
  try {
-    const {data}= await api.post('/projects',formData)
-    console.log(data)
+    const {data}= await api.post('/projects',formData)    
     return data
 
  } catch (error) {
-    console.log(error)
+   if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.message)
+   }
+    
  }
 }
